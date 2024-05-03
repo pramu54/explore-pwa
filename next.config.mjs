71b4c8acba6df31cb9c51b4895f750f7e3d8d0e7
pramu54 <mohadipramudiono@gmail.com>
2024-05-03 15:41:22 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import withPWAInit from '@ducanh2912/next-pwa';
+
 /** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+    dest: 'public',
+    cacheOnFrontEndNav : true,
+    aggresiveFrontEndNavCaching : true,
+    reloadOnOnline : true,
+    swcMinify : true,
+    disable : false,
+    workboxOptions: {
+        disableDevLogs: true,
+    }
+})
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
